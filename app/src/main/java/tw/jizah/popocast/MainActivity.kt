1923e86fdecd0ef259e96db84a1f608cfe9455e4
Joseph Wang <joseph_wang@ettoday.net>
2020-11-20 17:40:55 +0800

    Remove unused imports

--- app/src/main/java/tw/jizah/popocast/MainActivity.kt
+++ app/src/main/java/tw/jizah/popocast/MainActivity.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.setContent
-import androidx.ui.tooling.preview.Preview
-import tw.jizah.popocast.ui.player.PlayerController
 import tw.jizah.popocast.ui.theme.PoPoCastTheme
 
 class MainActivity : AppCompatActivity() {
