04be46b23db2ad1490db444c9fd7e020d8c66c18
AmyPlace <43088818+AmyPlace@users.noreply.github.com>
2020-12-02 18:30:46 +0800

    Implement episode date logic (#14)
    * Implement episode date logic
    * Change date string resource

---
+++ app/src/main/java/tw/jizah/popocast/utils/DateTimeUtils.kt
@@ -0,0 +1,56 @@
+package tw.jizah.popocast.utils
+
+import android.content.Context
+import tw.jizah.popocast.R
+import java.time.*
+import java.time.format.TextStyle
+import java.util.*
+
+object DateTimeUtils {
+
+    fun getDateString(context: Context, millisecond: Long = Instant.now().toEpochMilli()): String {
+        val releaseLocalDate = millisecond.getLocalDate()
+        val todayLocalDate = Instant.now().toEpochMilli().getLocalDate()
+        val yesterdayLocalDate = todayLocalDate.minusDays(1)
+        val beforeOneWeekMilliSeconds = todayLocalDate.minusWeeks(1).getMilliSecond()
+
+        return when {
+            beforeOneWeekMilliSeconds > millisecond -> {
+                val month = releaseLocalDate.month.getDisplayName(
+                    TextStyle.SHORT_STANDALONE,
+                    Locale.getDefault()
+                )
+                val isThisYear = (todayLocalDate.year == releaseLocalDate.year)
+                if (isThisYear) {
+                    context.getString(R.string.date_without_year, month, releaseLocalDate.dayOfMonth)
+                } else {
+                    context.getString(R.string.date_with_year, month, releaseLocalDate.dayOfMonth, releaseLocalDate.year)
+                }
+            }
+            yesterdayLocalDate.getMilliSecond() > millisecond -> {
+                releaseLocalDate.dayOfWeek.getDisplayName(
+                    TextStyle.SHORT_STANDALONE,
+                    Locale.getDefault()
+                )
+            }
+            releaseLocalDate == yesterdayLocalDate -> {
+                context.getString(R.string.yesterday)
+            }
+            else -> {
+                context.getString(R.string.today)
+            }
+        }
+    }
+
+
+    private fun Long.getLocalDate(): LocalDate {
+        return Instant.ofEpochMilli(this).atZone(ZoneId.systemDefault()).toLocalDate()
+    }
+
+    private fun LocalDate.getMilliSecond(): Long {
+        return atStartOfDay().getEpochMilliSecond()
+    }
+
+    fun LocalDateTime.getEpochMilliSecond(): Long =
+        atZone(ZoneId.systemDefault()).toInstant().epochSecond * 1000L
+}
